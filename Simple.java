054ae951f4bc8e6fa58597dbdb1e5439a351e563
srrreee <sridevir279@gmail.com>
2021-06-29 15:02:35 -0400

    Simple java prog

--- Simple.java
+++ Simple.java
@@ -1,4 +1,5 @@
 public class Simple{
 public static void main(String args[]){
-System.out.println("hello javatpoint");
+System.out.println("hello javatpointhffhfj");
+System.out.println("hello javatpointhffhfj");
 }}
